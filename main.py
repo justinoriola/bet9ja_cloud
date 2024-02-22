81764c1ada23146c47c4b4f7c67521bf9e74861c
justinoriola <justinoriola@yahoo.co.uk>
2024-02-22 21:59:31 +0000

    stable version2

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 
 from datetime import datetime, timedelta, time
+from notification_handler import NotificationHandler
 from spreadsheet_handler import SpreadSheetHandler
-from account_handler import AccountHandler
+from account_handler import AccountHandler, ACCOUNT_CREDENTIALS
 from logic_handler import LogicHandler
-import os
-from tabulate import tabulate
 from dotenv import load_dotenv
 load_dotenv(dotenv_path='.env')
+from time import sleep
 
 # Constants
 DAYS_OF_THE_WEEK = ['Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday']
@@ -14,134 +14,110 @@ CURRENT_TIME = datetime.now().time()
 TODAY = datetime.now().strftime('%A')
 TODAYS_DATE = datetime.now().date().strftime("%d/%m/%Y")
 PAST_DATE = (datetime.now().date() - timedelta(days=6)).strftime("%d/%m/%Y")
+message_bank = ''
 
-account_credentials = {
-    'ogba':{
-        'username': os.getenv('USERNAME1'),
-        'password': os.getenv('PASSWORD'),
-        'amount_to_credit': 40000,
-        'number_of_cashier': 4,
-        'base_balance':1000000,
-        'worksheet_name': "ogba_daily_spreadsheet",
-        'worksheet_id': os.getenv('SPREADSHEET_ID1')
-    },
-    'idiaraba':{
-        'username': os.getenv('USERNAME2'),
-        'password': os.getenv('PASSWORD'),
-        'amount_to_credit': 40000,
-        'number_of_cashier': 4,
-        'base_balance':1000000,
-        'worksheet_name': "idiaraba_daily_spreadsheet",
-        'worksheet_id': os.getenv('SPREADSHEET_ID2')
-    },
-    'awori': {
-        'username': os.getenv('USERNAME3'),
-        'password': os.getenv('PASSWORD'),
-        'amount_to_credit': 30000,
-        'number_of_cashier': 3,
-        'base_balance':700000,
-        'worksheet_name': "awori_daily_spreadsheet",
-        'worksheet_id': os.getenv('SPREADSHEET_ID3')
-    },
-    'moshalashi': {
-        'username': os.getenv('USERNAME4'),
-        'password': os.getenv('PASSWORD'),
-        'amount_to_credit': 10,
-        'number_of_cashier': 3,
-        'base_balance': 650000,
-        'worksheet_name': "moshalashi_daily_spreadsheet",
-        'worksheet_id': os.getenv('SPREADSHEET_ID4'),
-    },
-
-}
-
-# Main script
 if __name__ == '__main__':
     # set a start time
     start_time = datetime.now()
 
+    # instantiate notification object
+    notification_handler = NotificationHandler()
+
     # Loop through the credential dictionary's key
-    for key in account_credentials.keys():
+    for account_key in ACCOUNT_CREDENTIALS.keys():
 
-        # Unpacking account credentials
-        username = account_credentials[key]['username']
-        password = account_credentials[key]['password']
-        amount_to_credit = account_credentials[key]['amount_to_credit']
-        number_of_cashier = account_credentials[key]['number_of_cashier']
-        sheet_name = account_credentials[key]['worksheet_name']
-        sheet_id = account_credentials[key]['worksheet_id']
-        base_balance = account_credentials[key]['base_balance']
-
-        # Instantiate account handler objects
-        account = AccountHandler(username, password, amount_to_credit, number_of_cashier)
+        # Instantiate account handler object
+        account_handler = AccountHandler(account_key)
 
         # Withdraw and credit cashier account
-        if time(10, 0) <= CURRENT_TIME <= time(23, 0):
-            account.withdraw_from_cashier()
+        if time(20, 30) <= CURRENT_TIME <= time(21, 00):
+            cashier_checks = account_handler.cashier_check(account_handler.cashier_numbers)
+            account_handler.withdraw_from_cashier()
 
         # Instantiate spreadsheet handler objects and get spreadsheet values
-        if time(10, 0) <= CURRENT_TIME <= time(23, 0):
-            try:
-                # Instantiate handler object and get spreadsheet values
-                handler = SpreadSheetHandler(sheet_id, sheet_name, username, password, amount_to_credit, number_of_cashier)
-                spreadsheet_values = handler.get_spreadsheet_values()
+        if time(20, 30) <= CURRENT_TIME <= time(21, 00):
+            sleep(2)
 
-                # Unpack spreadsheet values
-                cash_float = int(spreadsheet_values['total_float'][0].replace(',', ''))
-                expenses = int(spreadsheet_values['total_expenses'][0].replace(',', ''))
-                banking = int(spreadsheet_values['total_banking'][0].replace(',', ''))
-                closing_balance = int(spreadsheet_values['closing_balance'][0].replace(',', ''))
-                bet_paid = spreadsheet_values['bet_ids']
-                already_paid_bet = spreadsheet_values['already_paid_bet']
+            # Instantiate handler object and get spreadsheet values
+            spreadsheet_handler = SpreadSheetHandler(account_key)
 
-                # filter bet_paid list
-                filtered_betlist = handler.filtered_bet_list(bet_paid, already_paid_bet)
+            # filter bet_paid list
+            bet_ticket_checks = spreadsheet_handler.betid_checks(
+                spreadsheet_handler.bet_paid, spreadsheet_handler.already_paid_bet
+            )
 
-                # Pay out bet slips
-                paid_betlist = account.bet_payout(filtered_betlist)
-                handler.upload_to_google_sheets(paid_betlist, already_paid_bet)
-                if not paid_betlist:
-                    account.login()
-                winning = account.winning_balance()
+            # Pay out bet slips and upload betIDs to the spreadsheet
+            paid_betlist = account_handler.bet_payout(spreadsheet_handler.bet_paid)
+            spreadsheet_handler.upload_to_google_sheets(paid_betlist, spreadsheet_handler.already_paid_bet)
 
-                # Instantiate logic
-                logic = LogicHandler()
-                admin_balance = account.get_admin_balance()
+            # Get admin winning/balance
+            winning = account_handler.winning_balance()
+            admin_balance = account_handler.get_admin_balance()
 
-                account_balance = logic.account_balance(base_balance=base_balance, expenses=expenses, banking=banking,
-                                                        closing_balance=closing_balance, float=cash_float,
-                                                        winning=winning, admin_balance=admin_balance)
+            # Instantiate logic handler object
+            logic = LogicHandler(
+                base_balance=account_handler.base_balance, expenses=spreadsheet_handler.expenses,
+                banking=spreadsheet_handler.banking, closing_balance=spreadsheet_handler.today_closing_balance,
+                float=spreadsheet_handler.cash_float, winning=winning, admin_balance=admin_balance
+            )
 
-                # tabulate the output and make it presentable
-                print(tabulate(logic.table(base_balance=base_balance, expenses=expenses, banking=banking,
-                                                        closing_balance=closing_balance, float=cash_float,
-                                                        winning=winning, admin_balance=admin_balance),
-                                                        headers=[username, key]))
+            account_balance = logic.account_balance(
+                base_balance=account_handler.base_balance, expenses=spreadsheet_handler.expenses, banking=spreadsheet_handler.banking,
+                closing_balance=spreadsheet_handler.today_closing_balance, float=spreadsheet_handler.cash_float,
+                winning=winning, admin_balance=admin_balance
+            )
+            account_handler.driver.quit()
 
-                dot_line_length = len(logic.account_status(account_balance))
-                print('-' * dot_line_length)
-                print(logic.account_status(account_balance))
-                print('-' * dot_line_length)
+            account_reset_message = logic.account_reset(
+                base_balance=account_handler.base_balance, winning=winning,
+                closing_balance=spreadsheet_handler.today_closing_balance,
+                admin_balance=admin_balance
+            )
 
-                # Create duplicate spreadsheet and reset existing spreadsheet for re-use.
-                if TODAY == "Monday" and time(00, 0) <= CURRENT_TIME <= time(00, 59):
-                    new_sheet_name = f'{sheet_name}: {PAST_DATE} - {TODAYS_DATE}'
-                    handler.create_spreadsheet_duplicate(new_sheet_name)
-                    for days_to_clear in DAYS_OF_THE_WEEK:
-                        handler.clear_spreadsheet(days_to_clear)
-                else:
-                    print(f'\nspreadsheet duplicate/clear function not schedule to run by this time')
-            except Exception as e:
-                print(f'An error occurred: {e}')
-            # finally:
-            #     account.driver.quit()
+            # print account balance/reset status
+            account_status_message = logic.account_status(account_balance)
+            # message_account_reset = f'Reset amount:' + f'{str(account_reset)}'
 
-        elif time(5, 0) <= CURRENT_TIME <= time(13, 59):
-            admin_balance = account.get_admin_balance()
-            account.credit_cashier(admin_balance)
+            # perform various checks: BetIDs, Opening/Closing balance and Cashier_withdrawal checks
+            message_account_checks = (
+                  f'\nChecks performed:'
+                  f'\n-{spreadsheet_handler.betid_checks(spreadsheet_handler.bet_paid, spreadsheet_handler.already_paid_bet)}'
+                  f'\n-{spreadsheet_handler.opening_balance_check(spreadsheet_handler.today_opening_balance, spreadsheet_handler.yesterday_closing_balance)}'
+                  f'\n-{cashier_checks}'
+            )
+            # instantiate and get the message to be sent
+            message = logic.text(
+                account_handler.admin_username, account_key, base_balance=account_handler.base_balance,
+                expenses=spreadsheet_handler.expenses, banking=spreadsheet_handler.banking,
+                closing_balance=spreadsheet_handler.today_closing_balance, float=spreadsheet_handler.cash_float,
+                winning=winning, admin_balance=admin_balance
+            )
+
+            # send report to WhatsApp
+            message += f'\nAccount Status:' + f"\n{'-' * 15}|" + f'\n{account_status_message}' + f'\n{account_reset_message}' + f"\n{'-' * 30}"
+            if account_balance != 0:
+                message += message_account_checks
+
+            # bank messages
+            message_bank += f'{message}'
+
+            # send Notification / print to console
+            print(message_bank)
+            notification_handler.send_sms(message)
+
+            # Create duplicate spreadsheet and reset existing spreadsheet for re-use.
+            if TODAY == "Monday" and time(20, 30) <= CURRENT_TIME <= time(21, 00):
+                new_sheet_name = f'{account_handler.sheet_name}: {PAST_DATE} - {TODAYS_DATE}'
+                spreadsheet_handler.create_spreadsheet_duplicate(new_sheet_name)
+
+        # credit cashier account at specified time
+        elif time(6, 0) <= CURRENT_TIME <= time(6, 30):
+            admin_balance = account_handler.get_admin_balance()
+            account_handler.credit_cashier(admin_balance)
         else:
             print('This other section(s) of the script is not scheduled to run by this time')
             print(f'CURRENT_TIME: {CURRENT_TIME}')
+            sleep(5)
 
     # calculate and print the total time taken to process the entire script
     end_time = datetime.now()
--- spreadsheet_handler.py
+++ spreadsheet_handler.py
@@ -9,25 +9,18 @@ load_dotenv(dotenv_path='.env')
 from datetime import datetime, timedelta, time
 from account_handler import AccountHandler
 
-
 # Constants
 MY_EMAIL = os.environ.get('MY_EMAIL')
 GOOGLE_API_SCOPES = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
-SERVICE_ACCOUNT_FILE = 'old-credentials.json'
+SERVICE_ACCOUNT_FILE = 'credentials.json'
 MAX_RETRIES = 5
 CURRENT_TIME = datetime.now().time()
 
-
-
-
 class SpreadSheetHandler(AccountHandler):
 
-    def __init__(self, spreadsheet_id, spreadsheet_name, admin_username: str, admin_password: str, amount_to_credit: int,
-                 total_number_of_cashiers: int):
-        super().__init__(admin_username, admin_password, amount_to_credit, total_number_of_cashiers)
+    def __init__(self,account_key):
 
-        self.spreadsheet_id = spreadsheet_id
-        self.spreadsheet_name = spreadsheet_name
+        super().__init__(account_key)
         self.scope = GOOGLE_API_SCOPES
         self.service_account_file = SERVICE_ACCOUNT_FILE
         self.spreadsheet_data = {}
@@ -35,14 +28,24 @@ class SpreadSheetHandler(AccountHandler):
         self.service_sheet = build('sheets', 'v4', credentials=self.credentials)
         self.service_drive = build('drive', 'v3',credentials=self.credentials)
         self.today = datetime.now().strftime('%A')
+        self.yesterday = (datetime.now() - timedelta(days=1)).strftime('%A')
         self.retry_counter = 0
+        self.spreadsheet_values = self.get_spreadsheet_values()
+        self.cash_float = int(self.spreadsheet_values['total_float'][0].replace(',', ''))
+        self.expenses = int(self.spreadsheet_values['total_expenses'][0].replace(',', ''))
+        self.banking = int(self.spreadsheet_values['total_banking'][0].replace(',', ''))
+        self.today_closing_balance = int(self.spreadsheet_values['today_closing_balance'][0].replace(',', ''))
+        self.today_opening_balance = int(self.spreadsheet_values['today_opening_balance'][0].replace(',', ''))
+        self.yesterday_closing_balance = int(self.spreadsheet_values['yesterday_closing_balance'][0].replace(',', ''))
+        self.bet_paid = self.spreadsheet_values['bet_ids']
+        self.already_paid_bet = self.spreadsheet_values['already_paid_bet']
 
     def get_credentials(self):
         return service_account.Credentials.from_service_account_file(self.service_account_file, scopes=self.scope)
 
     # Select spreadsheet day based on script run time
     def closing_balance(self):
-        if time(00, 0) <= CURRENT_TIME <= time(20, 29):
+        if time(00, 0) <= CURRENT_TIME <= time(10, 29):
             closing_balance_day = datetime.now() - timedelta(days=1)
             return closing_balance_day.strftime('%A')
         else:
@@ -50,26 +53,28 @@ class SpreadSheetHandler(AccountHandler):
 
     def bet_day(self, day_of_the_week):
         day_ranges = {
-            'Tuesday': '!N6:N56',
-            'Wednesday': '!O6:O56',
-            'Thursday': '!P6:P56',
-            'Friday': '!R6:R56',
-            'Saturday': '!O6:O56',
-            'Sunday': '!S6:S56',
-            'Monday': '!T6:T56'
+            'Tuesday': '!N6:N46',
+            'Wednesday': '!O6:O46',
+            'Thursday': '!P6:P46',
+            'Friday': '!Q6:Q46',
+            'Saturday': '!R6:R46',
+            'Sunday': '!S6:S46',
+            'Monday': '!T6:T46'
         }
         return 'Weekly Report' + day_ranges.get(day_of_the_week, '')
 
     def get_spreadsheet_values(self):
         print(f'\nRetrieving spreadsheet value(s) for account {self.admin_username}...')
-
+        bet_paid_range = self.bet_day(self.today)
         # Data
         WEEKLY_REPORT_RANGE = {
             'total_float': 'Weekly Report' + '!G15',
             'total_expenses': 'Weekly Report' + '!I15',
             'total_banking': 'Weekly Report' + '!k15',
-            'bet_ids': 'Weekly Report' + '!N6:T56',
-            'closing_balance': f'{self.closing_balance()}' + '!G19',
+            'bet_ids': bet_paid_range,
+            'today_closing_balance': f'{self.today}' + '!F11',
+            'today_opening_balance': f'{self.today}' + '!C28',
+            'yesterday_closing_balance': f'{self.yesterday}' + '!F11',
             'already_paid_bet': 'Weekly Report' + '!W6:W100',
         }
         # Calculate the delay between each request to avoid exceeding rate limit (60 requests per minute)
@@ -77,10 +82,11 @@ class SpreadSheetHandler(AccountHandler):
 
         # Loop over the weekly report range
         for key, value in WEEKLY_REPORT_RANGE.items():
+
             for i in range(MAX_RETRIES):
                 sleep(delay)
                 try:
-                    response = self.service_sheet.spreadsheets().values().get(spreadsheetId=self.spreadsheet_id,
+                    response = self.service_sheet.spreadsheets().values().get(spreadsheetId=self.sheet_id,
                                                                               range=value).execute()
                     values = response.get('values', [])
                     result = [item[0].strip() for item in values]
@@ -88,32 +94,34 @@ class SpreadSheetHandler(AccountHandler):
                         result = [value.strip() for sublist in values for value in sublist if isinstance(value, str)
                               and value.lower().startswith('b')]
                     self.spreadsheet_data[key] = result
+                    # print('quick nap cos of api limit...')
+                    sleep(2)
                     break
                 except HttpError as error:
                     if error.resp.status == 429:
                         print(f'API rate limit exceeded. Retrying in {i ** 2} seconds.')
                         sleep((i ** 2) + random.random())
+                except Exception as e:
+                    if self.retry_counter == 0:
+                        print(f'Something went wrong. Retrying in 120 seconds.')
+                        sleep(120)
+                        self.get_spreadsheet_values()
+                        self.retry_counter += 1
                     else:
-                        if self.retry_counter == 0:
-                            print(f'Something went wrong. Retrying in {120} seconds.')
-                            sleep(120)
-                            self.get_spreadsheet_values()
-                            self.retry_counter += 1
-                        else:
-                            print(f'Unable to retrieve spreadsheet value for {key}: {value}')
-                            print(f'HTTP error occurred: {error}')
-                            break
-        print(f'All spreadsheet values retrieved successfully')
-
+                        print(f'Unable to retrieve spreadsheet value for {key}: {value}')
+                        print(f'HTTP error occurred: {e}')
+                        break
+                finally:
+                    if 'already_paid_bet' in self.spreadsheet_data:
+                        print(f'All spreadsheet values retrieved successfully\n')
         return self.spreadsheet_data
-
 
     def create_spreadsheet_duplicate(self, duplicate_spreadsheet_name):
 
         try:
             # Create a request for duplicating the Spreadsheet
             request = self.service_drive.files().copy(
-                fileId=self.spreadsheet_id,
+                fileId=self.sheet_id,
                 body={"name": duplicate_spreadsheet_name}
             ).execute()
             copied_file_id = request.get('id')
@@ -133,46 +141,32 @@ class SpreadSheetHandler(AccountHandler):
         except Exception as error:
             print(f'An error occurred: {error}')
 
-
-
     def clear_spreadsheet(self, days_to_clear):
 
         """Clears specific cells of a spreadsheet for a given day."""
         # Data - cells in spreadsheet to be cleared
         cells_to_clear = {
-            f'{days_to_clear}!C10:E19': [['0', '0', '0']] * 10,
-            f'{days_to_clear}!G10:G19': [['0']] * 10,
-            f'{days_to_clear}!K6:K22': [['x']] * 17,
-            f'{days_to_clear}!L6:L22': [['0']] * 17,
-            f'{days_to_clear}!M6:M22': [['x']] * 17,
-            f'{days_to_clear}!N6:N22': [['0']] * 17,
-            f'{days_to_clear}!O6:O22': [['x']] * 17,
-            f'{days_to_clear}!P6:P22': [['0']] * 17,
-            f'{days_to_clear}!C32:C56': [['0']] * 25,
-            f'{days_to_clear}!D33:D56': [['x']] * 24,
-            f'{days_to_clear}!E32:E56': [['0']] * 25,
-            f'{days_to_clear}!F33:F56': [['x']] * 24,
-            f'{days_to_clear}!G32:G56': [['0']] * 25,
-            f'{days_to_clear}!L33:L57': [['0']] * 25,
-            f'{days_to_clear}!N33:N57': [['0']] * 25,
-            f'{days_to_clear}!P33:P57': [['0']] * 25,
-            f'{days_to_clear}!M34:M57': [['x']] * 24,
-            f'{days_to_clear}!O34:O57': [['x']] * 24,
+            f'{days_to_clear}!C10:F11': [['0', '0', '0', '0']] * 2,
+            f'{days_to_clear}!C18:C28': [['0']] * 11,
+            f'{days_to_clear}!F18:F29': [['0']] * 12,
+            f'{days_to_clear}!E23:E29': [['x']] * 7,
+            f'{days_to_clear}!B23:B28': [['x']] * 6,
+            f'{days_to_clear}!L4:L44': [['0']] * 41,
+            f'{days_to_clear}!K4:L44': [['x']] * 41,
+            f'{days_to_clear}!I6:I14': [['in progress...']] * 1,
         }
 
         # Loop over the cells to be cleared
         for cell_range, new_values in cells_to_clear.items():
             for i in range(MAX_RETRIES):
                 try:
-
                     # Prepare the request body with the new values
                     body = {
                         'values': new_values
                     }
-
                     # Send the update request
                     result = self.service_sheet.spreadsheets().values().update(
-                        spreadsheetId=self.spreadsheet_id,
+                        spreadsheetId=self.sheet_id,
                         range=cell_range,
                         valueInputOption='USER_ENTERED',
                         body=body).execute()
@@ -204,7 +198,7 @@ class SpreadSheetHandler(AccountHandler):
 
                     # Resize the sheet based on the length of the values_list
                     result = self.service_sheet.spreadsheets().values().update(
-                        spreadsheetId=self.spreadsheet_id,
+                        spreadsheetId=self.sheet_id,
                         range=range_,
                         body={'values': [[value] for value in value_list1]},
                         valueInputOption='RAW'
@@ -219,15 +213,37 @@ class SpreadSheetHandler(AccountHandler):
                         print(f'HTTP error occurred: {error}')
                         break
 
-    def filtered_bet_list(self, bet_list1: list, bet_list2: list) -> list:
-        print('\nfiltering bet list...')
+    def betid_checks(self, bet_list1: list, bet_list2: list):
+        print("Checking bet-ids for incorrect input or already paid ticket(s)...")
         try:
-            filtered_bet_list = [item for item in bet_list1 if item not in bet_list2]
-            if len(filtered_bet_list) == 0:
-                print('no new bet ticket(s) found')
-            else:
-                print(f'{len(filtered_bet_list)} new bet tickets found')
-            return filtered_bet_list
-        except Exception as error:
-            print(f'error occurred: {error}')
+            previously_paid_tickets = [item for item in bet_list1 if item in bet_list2]
+            incorrect_ticket_input = [ticket for ticket in bet_list1 if len(ticket) != 22]
 
+            if previously_paid_tickets and incorrect_ticket_input:
+                message = (f'{len(previously_paid_tickets)} previously paid tickets found:\n{", ".join(previously_paid_tickets)}\n'
+                        f'{len(incorrect_ticket_input)} incorrect ticket input:\n{", ".join(incorrect_ticket_input)}')
+
+            # Display previously paid tickets
+            if previously_paid_tickets:
+                message = f'{len(previously_paid_tickets)} previously paid tickets found:\n{", ".join(previously_paid_tickets)}'
+
+            # Display tickets with wrong input
+            if incorrect_ticket_input:
+                message = f'{len(incorrect_ticket_input)} incorrect ticket input:\n{", ".join(incorrect_ticket_input)}'
+
+            # Display message if no anomalies found
+            if not previously_paid_tickets and not incorrect_ticket_input:
+                message = 'no incorrect or already paid ticket!'
+        except Exception as error:
+            print(f'Error occurred: {error}')
+        finally:
+            return message
+
+    def opening_balance_check(self, today_opening_balance, yesterday_balance):
+        try:
+            if yesterday_balance == today_opening_balance:
+                return f"yday's closing = today's opening = {yesterday_balance}"
+            else:
+                return f"today's opening != yday's closing: {abs(yesterday_balance - today_opening_balance)} shortage."
+        except Exception as e:
+            print(f'Error occurred: {e}')
